96d2109385c6f5449f6971ec327ac060b7b6f2d2
Rene Koch <renekoch@hotmail.com>
2018-07-16 14:20:05 +0200

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,19 @@
+define(function () {
+	return function (timer) {
+		var my_promise = new Promise(function (resolve, reject) {
+      var timeout = setTimeout(function(){
+        timeout = 0;
+        resolve(this, timer);
+      }, timer);
+
+      my_promise.stopTimer = function(reason){
+        if (timeout) return;
+        clearTimeout(timeout);
+        timeout = 0;
+        reject(reason);
+      };
+    });
+
+    return my_promise;
+  }
+});
