2350568e635b2a08d63746e3ee0a06047ee417d1
Rene Koch <renekoch@hotmail.com>
2018-07-17 09:14:17 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,21 +2,23 @@ define(function () {
 	return function (timer) {
 		
 		var result = {};
-		
+		var _reject;
+		var timeout;
 		var my_promise = new Promise(function (resolve, reject) {
-      var timeout = setTimeout(function(){
-        timeout = 0;
-        resolve(this, timer);
-      }, timer);
+			timeout = setTimeout(function(){
+				timeout = 0;
+				resolve(this, timer);
+			}, timer);
+			_reject = reject;
 
-      my_promise.stopTimer = function(reason){
-        if (timeout) return;
-        clearTimeout(timeout);
-        timeout = 0;
-        reject(reason);
-      };
-    });
+		});
+		my_promise.stopTimer = function(reason){
+			if (timeout) return;
+			clearTimeout(timeout);
+			timeout = 0;
+			_reject(reason);
+		};
 
-    return my_promise;
-  }
+    		return my_promise;
+	}
 });
